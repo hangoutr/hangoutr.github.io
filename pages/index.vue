1b0adb225b9790635dd008c1fca3fc86b70be40a
Noppakun Wongsrinoppakun <tae8838@gmail.com>
2019-12-14 21:17:59 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        hangoutr
+      </h1>
+      <div class="links">
+        <img src="~/assets/ios_logo.png">
+        <img src="~/assets/android_logo.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+  margin: 50px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+img {
+  width: 230px;
+  height: 75px;
+}
+</style>
